9f694df8d435c467a200474baa17eff80107e682
jeff <jeffpeng2012@gmail.com>
2024-09-08 20:53:51 +0800

    feat: add frame.h that share in firmware and driver
    this file need a way to sync between firmware and driver

--- firmware/esp32-s3/include/frame.h
+++ firmware/esp32-s3/include/frame.h
@@ -1,13 +1,47 @@
 #include <stdint.h>
-#include  "freertos/queue.h"
-typedef struct _frame
+
+// 0:uart, 1:i2c, 2:spi, 3:can
+// first byte is the type of the frame
+typedef struct _UIO_frame
 {
-    bool isConfig;
-    uint8_t subsystem; // 0:uart, 1:i2c, 2:spi
-    union
-    {
-        uint8_t *data;
-        uint8_t *setting;
-    };
-    
-} frame;
+    uint8_t len;
+    uint8_t *data;
+} UIO_frame;
+
+typedef struct _UIO_uart_setting
+{
+    uint8_t type; // must be 0
+    uint8_t uart_num;
+    uint32_t baud_rate;
+    uint8_t data_bits;
+    uint8_t stop_bits;
+    uint8_t parity;
+    uint8_t flow_control;
+} UIO_uart_setting;
+
+typedef struct _UIO_i2c_setting
+{
+    uint8_t type; // must be 1
+    uint8_t i2c_num;
+    uint32_t clock_speed;
+    uint8_t address_mode;
+    uint8_t address;
+} UIO_i2c_setting;
+
+typedef struct _UIO_spi_setting
+{
+    uint8_t type; // must be 2
+    uint8_t spi_num;
+    uint32_t clock_speed;
+    uint8_t data_mode;
+    uint8_t data_order;
+    uint8_t cs_pin;
+} UIO_spi_setting;
+
+typedef struct _UIO_can_setting
+{
+    uint8_t type; // must be 3
+    uint8_t can_num;
+    uint32_t baud_rate;
+    uint8_t mode;
+} UIO_can_setting;
